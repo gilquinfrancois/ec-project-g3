bcb3cce8ccc90f563c34a9e43b9f6fd8bd24c4bb
Francois Gilquin <francoisgilquin@yahoo.fr>
2021-11-24 09:01:31 +0100

    structure projet

---
+++ src/main/java/be/heh/ecproject/EcProjectApplication.java
@@ -0,0 +1,13 @@
+package be.heh.ecproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/be/heh/ecproject/EcProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package be.heh.ecproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
