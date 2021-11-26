b67ce88e261f2b4a62d6605e372c91d3be6bde54
Francois Gilquin <francoisgilquin@yahoo.fr>
2021-11-26 16:28:44 +0100

    heroku commit

--- src/main/java/be/heh/ecproject/product/adapter/out/persistence/PersistenceAdapterConfiguration.java
+++ src/main/java/be/heh/ecproject/product/adapter/out/persistence/PersistenceAdapterConfiguration.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@Profile("dev")
+@Profile("prod")
 @Configuration
 @EnableJpaRepositories
 public class PersistenceAdapterConfiguration {
